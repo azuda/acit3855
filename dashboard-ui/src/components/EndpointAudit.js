5209061c8b1c802d779de23aa18f588c7a56f602
azuda <aaronzhang587@gmail.com>
2024-03-18 09:10:50 -0700

    Sync indices between audit and data

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -5,8 +5,11 @@ export default function EndpointAudit(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null)
-	// const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
-	const rand_val = 0; // default to 0
+	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+	// const rand_val = 0; // default to 0
+
+    const [index, setIndex] = useState(null);
+    setIndex(rand_val);
 
     const getAudit = () => {
         fetch(`http://kafka-acit3855.eastus2.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
@@ -33,7 +36,7 @@ export default function EndpointAudit(props) {
         
         return (
             <div>
-                <h3>{props.endpoint}-{rand_val}</h3>
+                <h3>{props.endpoint}-{index}</h3>
                 {JSON.stringify(log)}
             </div>
         )
