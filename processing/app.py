a420dca2fa2a1bd56e12780b59c969c209531038
azuda <aaronzhang587@gmail.com>
2024-03-25 10:08:53 -0700

    Fix new sqlite file logic

--- processing/app.py
+++ processing/app.py
@@ -45,13 +45,7 @@ filename = app_config["datastore"]["filename"]
 # create sqlite file if it does not exist
 if not os.path.exists(filename):
   with open(filename, "w") as f:
-    data = {
-      "id": 1, 
-      "num_speed_readings": 0, 
-      "max_speed_reading": 0, 
-      "num_vertical_readings": 0, 
-      "max_vertical_reading": 0
-    }
+    data = []
     json.dump(data, f)
 
 # link to sqlite
