04e3f31471f281308fd4cd2feb40e8085f284bc0
azuda <aaronzhang587@gmail.com>
2024-04-03 16:47:47 -0700

    nosec

--- storage/create_tables_mysql.py
+++ storage/create_tables_mysql.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 
 # conn = mysql.connector.connect(host="localhost", user="root",
-#                               password="password", database="events")
+#                               password="password", database="events")  # nosec
 conn = mysql.connector.connect(host="kafka-acit3855.eastus2.cloudapp.azure.com",
-                              user="user", password="password", database="events")
+                              user="user", password="password", database="events")  # nosec
 
 cursor = conn.cursor()
 cursor.execute('''
