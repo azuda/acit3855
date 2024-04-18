330a9ec34e385b0dad6e5251606f3906f5e90e36
azuda <aaronzhang587@gmail.com>
2024-04-18 16:58:20 -0700

    Fix dashboard

--- dashboard-ui/src/components/EndpointAnomalies.js
+++ dashboard-ui/src/components/EndpointAnomalies.js
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react';
 
 function EndpointAnomalies() {
-    const [eventIds, setEventIds] = useState([]);
+    const [speedId, setSpeedId] = useState(null);
+    const [verticalId, setVerticalId] = useState(null);
 
     useEffect(() => {
-        fetch('http://kafka-acit3855.eastus2.cloudapp.azure.com:8130/anomalies')
+        fetch('http://kafka-acit3855.eastus2.cloudapp.azure.com:8130/anomalies?event_type=TooLow')
             .then(response => response.json())
             .then(data => {
-                const ids = data.map(event => event.result[0].event_id);
-                setEventIds(ids);
+                setSpeedId(data.result[0].event_id);
+            })
+            .catch(error => console.error('Error:', error));
+
+        fetch('http://kafka-acit3855.eastus2.cloudapp.azure.com:8130/anomalies?event_type=TooHigh')
+            .then(response => response.json())
+            .then(data => {
+                setVerticalId(data.result[0].event_id);
             })
             .catch(error => console.error('Error:', error));
     }, []);
 
     return (
         <div>
-            <h2>Latest Event UUIDs</h2>
-            {eventIds.map((id, index) => (
-                <p key={index}>Event {index + 1}: {id}</p>
-            ))}
+            <p>Speed Latest Anomaly UUID: {speedId}</p>
+            <p>Vertical Latest Anomaly UUID: {verticalId}</p>
         </div>
     );
 }
