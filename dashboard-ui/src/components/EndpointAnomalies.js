0149cdf7f44a2e24006cfade0e954efa25ffb662
azuda <aaronzhang587@gmail.com>
2024-04-18 17:12:18 -0700

    Fix dashboard

--- dashboard-ui/src/components/EndpointAnomalies.js
+++ dashboard-ui/src/components/EndpointAnomalies.js
@@ -8,14 +8,14 @@ function EndpointAnomalies() {
         fetch('http://kafka-acit3855.eastus2.cloudapp.azure.com:8130/anomalies?anomaly_type=TooLow')
             .then(response => response.json())
             .then(data => {
-                setSpeedId(data.result[0].event_id);
+                setSpeedId(data[0].event_id);
             })
             .catch(error => console.error('Error:', error));
 
         fetch('http://kafka-acit3855.eastus2.cloudapp.azure.com:8130/anomalies?anomaly_type=TooHigh')
             .then(response => response.json())
             .then(data => {
-                setVerticalId(data.result[0].event_id);
+                setVerticalId(data[0].event_id);
             })
             .catch(error => console.error('Error:', error));
     };
