edc157e5969411fbe0f7767e193ccdefdf1ce2ef
azuda <aaronzhang587@gmail.com>
2024-04-18 17:02:08 -0700

    Fix dashboard

--- dashboard-ui/src/components/EndpointAnomalies.js
+++ dashboard-ui/src/components/EndpointAnomalies.js
@@ -5,14 +5,14 @@ function EndpointAnomalies() {
     const [verticalId, setVerticalId] = useState(null);
 
     useEffect(() => {
-        fetch('http://kafka-acit3855.eastus2.cloudapp.azure.com:8130/anomalies?event_type=TooLow')
+        fetch('http://kafka-acit3855.eastus2.cloudapp.azure.com:8130/anomalies?anomaly_type=TooLow')
             .then(response => response.json())
             .then(data => {
                 setSpeedId(data.result[0].event_id);
             })
             .catch(error => console.error('Error:', error));
 
-        fetch('http://kafka-acit3855.eastus2.cloudapp.azure.com:8130/anomalies?event_type=TooHigh')
+        fetch('http://kafka-acit3855.eastus2.cloudapp.azure.com:8130/anomalies?anomaly_type=TooHigh')
             .then(response => response.json())
             .then(data => {
                 setVerticalId(data.result[0].event_id);
